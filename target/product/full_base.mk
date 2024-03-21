PixelExperience/build

build: Guard more lines related to AOSP ringtones
Change-Id: Ib01ae9abdf1b8fe9ece29485e9bc9820d0af48d9

target/product/full_base.mk
@@ -43,9 +43,11 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(CUSTOM_BUILD),)
 PRODUCT_VENDOR_PROPERTIES := \
     ro.config.ringtone?=Ring_Synth_04.ogg \
     ro.config.notification_sound?=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -53,8 +55,6 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 ifeq ($(CUSTOM_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
 # Get a list of languages.

target/product/handheld_system.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.carrier?=unknown \
+    ro.carrier?=unknown
+
+ifeq ($(CUSTOM_BUILD),)
+PRODUCT_VENDOR_PROPERTIES := \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
+endif

PixelBuilds-Staging/android_build

target/product/full_base.mk
@@ -37,9 +37,11 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(PB_BUILD),)
 PRODUCT_VENDOR_PROPERTIES := \
     ro.config.ringtone?=Ring_Synth_04.ogg \
     ro.config.notification_sound?=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -47,8 +49,6 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 ifeq ($(PB_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
 # Get a list of languages.

target/product/handheld_system.mk
@@ -88,6 +88,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.window_magnification.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.window_magnification.xml \
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.carrier?=unknown \
+    ro.carrier?=unknown
+
+ifeq ($(PB_BUILD),)
+PRODUCT_VENDOR_PROPERTIES := \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
+endif